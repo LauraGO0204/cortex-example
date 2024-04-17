96c462b8888adc189bfed42f5458aecee65681ff
LauraGO0204 <167275687+LauraGO0204@users.noreply.github.com>
2024-04-16 21:59:41 -0600

    Update CtxClient.cs

--- csharp/CortexAccess/CtxClient.cs
+++ csharp/CortexAccess/CtxClient.cs
@@ -542,7 +542,7 @@ namespace CortexAccess
             {
                 param.Add("license", licenseID);
             }
-            param.Add("debit", debitNumber);
+            param.Add("debit", debitNumber 1);
             SendTextMessage(param, "authorize", true);
         }
 
